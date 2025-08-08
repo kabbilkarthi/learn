4806f12b7269e767b652b145b9ea90a36d4a5e77
Kabbil Gi (External) <kabbil@synopsys.com>
2025-08-08 15:44:14 +0530

    in celsius

--- weather.py
+++ weather.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 def get_current_weather(city="Trichy"):
 
-    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
     weather_data = requests.get(request_url).json()
-
+    #print(request_url)
     return weather_data
 
 
